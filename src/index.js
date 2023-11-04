f97a8e03d93cf3742b3f8bf6125e3241395ce358
teanirudh <teanirudh@gmail.com>
2023-11-04 20:19:22 +0530

    Setup antd and tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="font-bold underline">Hello world!</h1>
     </div>
   );
 };
