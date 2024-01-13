6026330f66634fadbb9048e9bb687d780f17f106
teanirudh <teanirudh@gmail.com>
2024-01-13 20:18:33 +0530

    Setup sequelize

--- engine/db/index.js
+++
@@ -1,7 +0,0 @@
-import { is } from "@electron-toolkit/utils";
-
-const knex = require("knex");
-const knexfile = require("./knexfile");
-
-const db = is.dev ? knex(knexfile.development) : knex(knexfile.production);
-module.exports = db;
